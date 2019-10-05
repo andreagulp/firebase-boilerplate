694d3ae984618b3f98591435db24e9d52d99d164
andreagulp <andreacommunication@gmail.com>
2019-10-05 21:45:59 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import News from "./pages/News";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+const useStyles = makeStyles(theme => ({
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(0, 0, 6)
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <main>
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Switch>
+              <Route path="/news" component={News} />
+              <Route path="/signin" component={SignIn} />
+              <Route path="/signup" component={SignUp} />
+              <Route path="/" component={Home} />
+            </Switch>
+          </Container>
+        </div>
+      </main>
+    </>
   );
 }
 
